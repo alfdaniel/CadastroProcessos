5f6e93a4796b00900ca2e758619d07fb12503c03
Daniel Ferreira <daniel.alexandre22@hotmail.com>
2024-10-09 12:31:39 -0300

    ajuste validação npu

--- CadastroProcesso/Models/ProcessoModel.cs
+++ CadastroProcesso/Models/ProcessoModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace CadastroProcessos.Models
 {
@@ -10,8 +11,7 @@ namespace CadastroProcessos.Models
         [Required(ErrorMessage = "Campo Obrigatório"), MaxLength(120)]
         public string? NomeProcesso { get; set; }
         [Required(ErrorMessage = "Campo Obrigatório.")]
-        [MinLength(20, ErrorMessage = "O NPU deve ter no mínimo 20 caracteres.")]
-        [MaxLength(20, ErrorMessage = "O NPU deve ter no máximo 20 caracteres.")]
+        [CustomValidation(typeof(ProcessoModel), nameof(ValidateNpu))]
         public string? Npu { get; set; }
         [Required(ErrorMessage = "Campo Obrigatório")]
         public DateTime DataCadastro { get; set; }
@@ -23,6 +23,24 @@ namespace CadastroProcessos.Models
         [Required(ErrorMessage = "Campo Obrigatório")]
         public int? CodMunicipio { get; set; }
         public bool Visualizado { get; set; }
+
+        public static ValidationResult ValidateNpu(string? npu, ValidationContext context)
+        {
+            if (string.IsNullOrEmpty(npu))
+            {
+                return new ValidationResult("NPU não pode ser nulo ou vazio.");
+            }
+
+            // Remove caracteres não numéricos
+            npu = Regex.Replace(npu, @"\D", "");
+
+            if (npu.Length != 20)
+            {
+                return new ValidationResult("NPU deve conter exatamente 20 caracteres.");
+            }
+
+            return ValidationResult.Success!;
+        }
 
     }
 }
